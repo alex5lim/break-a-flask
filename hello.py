b4756df18e6ae572ff7f0276ddfc0f91f886ece3
Alexander Lim <alexander.halim+github@gmail.com>
2018-07-30 21:26:19 +0800

    Refactor templates to use inheritance from base.html and add 404/500 error pages.

--- hello.py
+++ hello.py
@@ -18,3 +18,13 @@ def user(name):
 def browser():
     user_agent = request.headers.get('User-Agent')
     return '<p>Your browser is {}</p>'.format(user_agent)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
